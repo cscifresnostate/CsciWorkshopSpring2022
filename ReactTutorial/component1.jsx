2421aab3db9248943046efb3f900ce5d4961cb04
Fresno State CSci Club <100539061+cscifresnostate@users.noreply.github.com>
2022-03-16 12:55:09 -0700

    Add files via upload

---
+++ ReactTutorial/component1.jsx
@@ -0,0 +1,18 @@
+import React, {component} from 'react';
+import '../assets/helloWorld.css'
+
+
+//Always capitalize the First letter. Case sensitive
+const HelloWorld = () =>{
+
+    return(
+        <div className="helloworld">
+
+            <h1>Hello World!</h1>
+            
+        </div>
+    )
+    
+}
+
+export default HellowWorld;
